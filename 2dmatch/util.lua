d30ae1a3500d9c97c869b82727a203726b74b4df
Matthias Niessner <niessner@cs.stanford.edu>
2017-02-08 22:20:55 -0800

    basic keypoint loader

--- 2dmatch/util.lua
+++ 2dmatch/util.lua
@@ -1,5 +1,64 @@
 -- Some useful random util functions 
 
+function loadMatchFile(file)
+	assert(paths.filep(file))
+
+	local keypoints = {}
+
+	local i = 0
+	for line in io.lines(file) do	
+		if i > 2 then
+			local parts = {}
+			for p in line:gmatch('([^\t]+)') do table.insert(parts, p) end
+			--print(parts)
+			table.insert(keypoints, parts)
+		
+			if i >= 10+2 then break end
+		end
+		i = i + 1
+	end
+
+	return keypoints
+
+end
+
+-- Includes: matching tote image and product image, and non-matching tote image
+function getTrainingExampleTriplet(path, kp_anc, kp_pos, kp_neg)
+
+	str_anc = string.format("color-%02d-%06d.jpg", kp_anc[2], kp_anc[3])
+	str_pos = string.format("color-%02d-%06d.jpg", kp_pos[2], kp_pos[3])
+	str_neg = string.format("color-%02d-%06d.jpg", kp_neg[2], kp_neg[3])
+
+	print(str_anc)
+
+    local matchImg = image.load(paths.concat(path,str_anc),3,'float')
+    local anchorImg = image.load(paths.concat(path,str_pos),3,'float')
+    local nonMatchImg = image.load(paths.concat(path,str_neg),3,'float')
+
+	--TODO continue here
+    -- Pixel locations of patch centers (x,y)
+    local matchPixelLoc = {}
+    local anchorPixelLoc = {808,897}
+    local nonMatchPixelLoc = {661,794}
+
+    -- Extract 64x64 patches
+    local patchSize = 64
+    local matchPatch = image.crop(matchImg,matchPixelLoc[1]-patchSize/2,matchPixelLoc[2]-patchSize/2,matchPixelLoc[1]+patchSize/2,matchPixelLoc[2]+patchSize/2)
+    local anchorPatch = image.crop(anchorImg,anchorPixelLoc[1]-patchSize/2,anchorPixelLoc[2]-patchSize/2,anchorPixelLoc[1]+patchSize/2,anchorPixelLoc[2]+patchSize/2)
+    local nonMatchPatch = image.crop(nonMatchImg,nonMatchPixelLoc[1]-patchSize/2,nonMatchPixelLoc[2]-patchSize/2,nonMatchPixelLoc[1]+patchSize/2,nonMatchPixelLoc[2]+patchSize/2)
+
+    -- Preprocess image patches
+    matchPatch = preprocessImg(matchPatch)
+    anchorPatch = preprocessImg(anchorPatch)
+    nonMatchPatch = preprocessImg(nonMatchPatch)
+
+    return matchPatch,anchorPatch,nonMatchPatch
+	--]]
+end
+
+
+
+
 -- Lookup filenames in directory (with search query string)
 function scanDir(directory,query)
     local i, t, popen = 0, {}, io.popen
@@ -32,31 +91,6 @@ function preprocessImg(img)
     -- mean_pixel = mean_pixel:view(3,1,1):expandAs(img)
     -- img:add(-1,mean_pixel)
     -- return img
-end
-
--- Includes: matching tote image and product image, and non-matching tote image
-function getTrainingExampleTriplet()
-    local matchImg = image.load(paths.concat('data','sample','frame-000001.color.jpg'),3,'float')
-    local anchorImg = image.load(paths.concat('data','sample','frame-000000.color.jpg'),3,'float')
-    local nonMatchImg = image.load(paths.concat('data','sample','frame-000000.color.jpg'),3,'float')
-
-    -- Pixel locations of patch centers (x,y)
-    local matchPixelLoc = {821,109}
-    local anchorPixelLoc = {808,897}
-    local nonMatchPixelLoc = {661,794}
-
-    -- Extract 64x64 patches
-    local patchSize = 64
-    local matchPatch = image.crop(matchImg,matchPixelLoc[1]-patchSize/2,matchPixelLoc[2]-patchSize/2,matchPixelLoc[1]+patchSize/2,matchPixelLoc[2]+patchSize/2)
-    local anchorPatch = image.crop(anchorImg,anchorPixelLoc[1]-patchSize/2,anchorPixelLoc[2]-patchSize/2,anchorPixelLoc[1]+patchSize/2,anchorPixelLoc[2]+patchSize/2)
-    local nonMatchPatch = image.crop(nonMatchImg,nonMatchPixelLoc[1]-patchSize/2,nonMatchPixelLoc[2]-patchSize/2,nonMatchPixelLoc[1]+patchSize/2,nonMatchPixelLoc[2]+patchSize/2)
-
-    -- Preprocess image patches
-    matchPatch = preprocessImg(matchPatch)
-    anchorPatch = preprocessImg(anchorPatch)
-    nonMatchPatch = preprocessImg(nonMatchPatch)
-
-    return matchPatch,anchorPatch,nonMatchPatch
 end
 
 -- Get subset of a 1D table
