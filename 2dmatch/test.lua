c4e6adc355726dc273f95850e5196e8afdef153e
angeladai <adai@cs.stanford.edu>
2017-02-19 17:48:22 -0800

    test: write out feature vectors for anc/pos/neg patches

--- 2dmatch/test.lua
+++ 2dmatch/test.lua
@@ -5,6 +5,8 @@ require 'image'
 require 'util'
 -- require 'qtwidget'
 
+-- example usage:  th test.lua --gpu_index 0 --model ~/data/matthias/Matterport/2dmatch/logs/model_20000.net --train_data none test_data scenes_test.txt
+
 local basePath = '/mnt/raid/datasets/Matterport/Matching/'
 local patchSize = 64
 
@@ -12,8 +14,8 @@ opt_string = [[
     -h,--help                                      			 print help
     --gpu_index     		(default 0)                      GPU index
 	--model					(default "model.net")		     torch model file path
-    --train_data            (default "scenes_train.txt")     txt file containing train
     --test_data             (default "scenes_test.txt")      txt file containing test
+    --train_data            (default "")     				 txt file containing train (optional)
 	--output_dir			(default "output")			 	 output dir for match distances
 ]]
 
@@ -32,6 +34,11 @@ end
 -- Set RNG seed
 --math.randomseed(os.time())
 math.randomseed(0)
+
+if (paths.dirp(opt.output_dir)) then
+	print(sys.COLORS.red .. 'warning: output dir ' .. opt.output_dir .. ' already exists, press key to continue' .. sys.COLORS.none)
+	io.read()
+end
 
 -- set gpu
 cutorch.setDevice(opt.gpu_index+1)
@@ -80,7 +87,8 @@ end
 local test_files = getDataFiles(paths.concat(basePath,opt.test_data))
 print(test_files)
 
-function test(data_files)
+function test(data_files, outpath)
+	assert(paths.dirp(outpath))
 	-- Set to testing mode (disable dropout and batch normalization)
 	patchEncoder:evaluate()
 	
@@ -88,6 +96,12 @@ function test(data_files)
 	local poss, negs = loadMatchFiles(basePath, data_files, patchSize/2)
 	
 	local inputs = {}
+	
+	-- output files
+	splitter = ',' --output as csv
+	local outfile_anc = assert(io.open(paths.concat(outpath, 'anc_feat.txt'), "w"))
+	local outfile_pos = assert(io.open(paths.concat(outpath, 'pos_feat.txt'), "w"))
+	local outfile_neg = assert(io.open(paths.concat(outpath, 'neg_feat.txt'), "w"))
 	
 	for k = 1,#poss do
 		-- print progress bar :D		
@@ -100,20 +114,43 @@ function test(data_files)
 		local anc,pos,neg = getTrainingExampleTriplet(imgPath, poss[k][2], poss[k][3], negs[k][3], patchSize)
 		
 		-- Compute features per patch
-		local ancFeat = patchEncoder:forward(anc:resize(1,3,224,224):cuda()):clone()
-		local posFeat = patchEncoder:forward(pos:resize(1,3,224,224):cuda()):clone()
-		local negFeat = patchEncoder:forward(neg:resize(1,3,224,224):cuda()):clone()
+		local ancFeat = torch.squeeze(patchEncoder:forward(anc:resize(1,3,224,224):cuda()):clone())
+		local posFeat = torch.squeeze(patchEncoder:forward(pos:resize(1,3,224,224):cuda()):clone())
+		local negFeat = torch.squeeze(patchEncoder:forward(neg:resize(1,3,224,224):cuda()):clone())
+		--print(ancFeat:size())
+		assert(ancFeat:size(1) == posFeat:size(1) and ancFeat:size(1) == negFeat:size(1))
+
+		-- -- descriptor distance between patches
+		--print('Distance between matching patches: ' ..       ancFeat:dist(posFeat))
+		--print('Distance between non-matching patches 1: ' .. posFeat:dist(negFeat))
+		--print('Distance between non-matching patches 2: ' .. ancFeat:dist(negFeat))
 		
-		-- descriptor distance between patches
-		print('Distance between matching patches: ' ..       ancFeat:dist(posFeat))
-		print('Distance between non-matching patches 1: ' .. posFeat:dist(negFeat))
-		print('Distance between non-matching patches 2: ' .. ancFeat:dist(negFeat))
-		
-		io.read() --TODO save this to a file
+		-- log to file
+		for i=1,ancFeat:size(1) do
+			outfile_anc:write(string.format("%.6f", ancFeat[i]))
+			outfile_pos:write(string.format("%.6f", posFeat[i]))
+			outfile_neg:write(string.format("%.6f", negFeat[i]))
+			if i < ancFeat:size(1) then 
+				outfile_anc:write(splitter)
+				outfile_pos:write(splitter)
+				outfile_neg:write(splitter)
+			end
+		end
+		outfile_anc:write('\n')
+		outfile_pos:write('\n')
+		outfile_neg:write('\n')
 	end
 end
 
 -- Test!
-if #train_files > 0 then test(train_files) end
-test(test_files)
-
+if not paths.dirp(opt.output_dir) then paths.mkdir(opt.output_dir) end
+if #train_files > 0 then
+	local outpath = paths.concat(opt.output_dir, 'train')
+	if not paths.dirp(outpath) then paths.mkdir(outpath) end
+	test(train_files, outpath)
+end
+do 
+	local outpath = paths.concat(opt.output_dir, 'test')
+	if not paths.dirp(outpath) then paths.mkdir(outpath) end
+	test(test_files, outpath)
+end
